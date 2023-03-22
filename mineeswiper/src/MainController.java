d07880029691f8bbef160d09ce9625578c2f5893
BraynerMoncada <93609232+BraynerMoncada@users.noreply.github.com>
2023-03-22 17:36:35 -0600

    Commit interfaz
    En este commit se realiza una interfaz sencilla con dos botones que llevan a otra ventana(Dummy y Advanced)

---
+++ mineeswiper/src/MainController.java
@@ -0,0 +1,35 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    @FXML
+    private void openDummyWindow() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("dummy.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void openAdvancedWindow() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("advanced.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
